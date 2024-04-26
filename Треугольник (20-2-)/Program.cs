1c9e43160d90366d9130b1c263973d3f059a6769
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-04-26 14:21:16 +0300

    Add files via upload

---
+++ Треугольник (20-2-)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Треугольник__20_2__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Треугольник (20-2-)/Triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Треугольник__20_2__
+{
+    class Triangle <T>
+    {
+        private T a;
+        private T b;
+        private T c;
+
+        public T A { get; set; }
+        public T B { get; set; }
+        public T C { get; set; }
+        public Triangle() { }
+        public Triangle(T a, T b, T c)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Сторона А: {A}\nСторона B: {B}\nСторона C: {C}");
+        }
+        public double Square(double a, double b, double c)
+        {
+            double per = a + b + c;
+            double p = per/2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+}
