d37c4fdb56b1009b1d05cfaf9ce3dcbb401ddb82
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-04-28 22:37:26 +0300

    Add files via upload

--- Треугольник (20-2-)/Program.cs
+++ Треугольник (20-2-)/Program.cs
@@ -10,7 +10,14 @@ namespace Треугольник__20_2__
     {
         static void Main(string[] args)
         {
+            Triangle<int> triangle = new Triangle<int>();
+            Console.WriteLine(triangle.Square(5, 6, 7));
+            Triangle<double> triangle1 = new Triangle<double>();
+            Console.WriteLine(triangle.Square(6.6, 4.3, 3.5));
+            Triangle<uint> triangle2 = new Triangle<uint>();
+            Console.WriteLine(triangle2.Square(200,267,120));
 
+            Console.ReadKey();
         }
     }
 }
--- Треугольник (20-2-)/Triangle.cs
+++ Треугольник (20-2-)/Triangle.cs
@@ -8,29 +8,66 @@ namespace Треугольник__20_2__
 {
     class Triangle <T>
     {
+        /// <summary>
+        /// Создание поля а
+        /// </summary>
         private T a;
+        /// <summary>
+        /// Создание поля b
+        /// </summary>
         private T b;
+        /// <summary>
+        /// Создание поля c
+        /// </summary>
         private T c;
-
+        /// <summary>
+        /// Свойства А
+        /// </summary>
         public T A { get; set; }
+        /// <summary>
+        /// Свойства В
+        /// </summary>
         public T B { get; set; }
+        /// <summary>
+        /// Свойства С
+        /// </summary>
         public T C { get; set; }
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
         public Triangle() { }
+        /// <summary>
+        /// Конструктор с параметрами 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
         public Triangle(T a, T b, T c)
         {
             this.A = a;
             this.B = b;
             this.C = c;
         }
+        /// <summary>
+        /// Метод для вывода площади треугольника 
+        /// </summary>
         public void Show()
         {
             Console.WriteLine($"Сторона А: {A}\nСторона B: {B}\nСторона C: {C}");
         }
-        public double Square(double a, double b, double c)
+        public string Square(dynamic a, dynamic b, dynamic c)
         {
-            double per = a + b + c;
-            double p = per/2;
-            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            if ((a + b) > c && (a + c) > b && (c + b) > a)
+            {
+                double per = a + b + c;
+                double p = per / 2;
+                return $"Треугольник существует.\nПлощадь: {Math.Sqrt(p * (p - a) * (p - b) * (p - c))}";
+            }
+            else
+            {
+                return "Треульник не существует";
+            }
         }
+
     }
 }
